9b088784091ae5f3f059054a06ef1f52dd48c188
Gavriil Papagavriil <papagav@gmail.com>
2018-09-04 23:53:11 +0300

    page 126

--- ClientApp/components/App.vue
+++ ClientApp/components/App.vue
@@ -1,8 +1,34 @@
 <template>
-    <router-view />
+    <div class="app">
+        <b-navbar toggleable="md" type="dark" variant="dark">
+            <b-container>
+                <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+                <b-navbar-brand to="/">PhoneShop</b-navbar-brand>
+                <b-collapse is-nav id="nav_collapse">
+                    <b-navbar-nav>
+                        <b-nav-item to="/products">Products</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-container>
+        </b-navbar>
+        <router-view />
+    </div>
 </template>
+
+
 <script>
 export default {
   name: "app"
 };
 </script>
+
+<style lang="scss">
+html,
+body {
+  height: 100vh;
+}
+div.app,
+div.page {
+  height: 100% !important;
+}
+</style>
--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -11,13 +11,13 @@ module.exports = (env) => {
         resolve: { extensions: [ '.js' ] },
         entry: {
             vendor: [
-                'bootstrap',
-                'bootstrap/dist/css/bootstrap.css',
                 'event-source-polyfill',
                 'isomorphic-fetch',
-                'jquery',
                 'vue',
-                'vue-router'
+                'vue-router',
+                "bootstrap/dist/css/bootstrap.min.css",
+                "bootstrap-vue",
+                "nprogress/nprogress.css"
             ],
         },
         module: {
